129e39431304927c402cb657c5c44e5f87a1e613
jatin311003 <ajatin3110@gmail.com>
2025-04-18 03:29:17 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,21 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+const { withNetlify } = require('@netlify/next');
+
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "randomuser.me",
-        },
-      ],
-    },
-  
-    experimental: {
-      serverActions: {
-        bodySizeLimit: "5mb",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
       },
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
     },
-  };
-  
-  export default nextConfig;
+  },
+};
+
+module.exports = withNetlify(nextConfig);
